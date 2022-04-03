698ae0c25aa47044357aceb4d3b6fd48e65a0b93
manaka6122 <manaka130130@gmail.com>
2022-04-03 12:06:34 +0000

    Add devise for membership function

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  root to: "public/homes#top"
+  get '/about' => 'public/homes#about', as: 'about'
+  namespace :public do
+
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
