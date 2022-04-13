f2dba1f02710219eec15766e8372ee10c788eeb2
manaka6122 <manaka130130@gmail.com>
2022-04-13 11:59:03 +0000

    Update activity index screen layout

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,7 +4,7 @@ class Public::CustomersController < ApplicationController
 
   def show
     @teams = @customer.teams.page(params[:page])
-    @activities = @customer.activities.page(params[:page])
+    @activities = @customer.activities.page(params[:page]).per(6)
   end
 
   def edit
